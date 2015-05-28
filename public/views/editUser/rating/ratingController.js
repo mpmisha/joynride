b4f0013a08f69b66b4f61fea981f478695640516
Michael Merjanov <michaelm@self-point.com>
2015-05-29 00:19:51 +0300

    'msg'

---
+++ public/views/editUser/rating/ratingController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Michael on 5/21/2015.
+ */
+angular.module('joynRideApp').controller('ratingController', function ($scope, $http, $window, $location, config, Request) {
+    console.log('toyooy')
+});
