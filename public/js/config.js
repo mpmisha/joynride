7e864e1437977194da2a8648f997bc219c2331fa
Michael Merjanov <michaelm@self-point.com>
2015-06-03 00:50:52 +0300

    back_to_http

--- public/js/config.js
+++ public/js/config.js
@@ -3,6 +3,6 @@
  */
 angular.module('joynRideApp')
     .constant('config', {
-        'baseUrl': 'https://80.178.198.187:8080/joynride'
+        'baseUrl': 'http://80.178.198.187:8080/joynride'
         //baseURL:'localhost:8080/joynride/'
     })
