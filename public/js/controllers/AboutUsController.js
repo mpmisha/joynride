f4f68422acf58497f0bc6ec73d7572a96bf57287
Michael Merjanov <michaelm@self-point.com>
2015-06-08 00:03:04 +0300

    design about us page

--- public/js/controllers/AboutUsController.js
+++ public/js/controllers/AboutUsController.js
@@ -3,4 +3,23 @@
  */
 angular.module('joynRideApp').controller('AboutController', function($scope) {
     $scope.message = 'Look! I am an about page.';
+    $scope.members=[
+        {
+            name:'Michael Merjanov',
+            text:'A dude who does things',
+            pic:'../img/michael.jpg'
+        },{
+            name:'Shany Radai',
+            text:'A girl that makes things happen',
+            pic:'../img/profile.jpg'
+        },{
+            name:'Daniel Litvak',
+            text:'A dude that does not sleep at all',
+            pic:'../img/profile.png'
+        },{
+            name:'Guy familyName',
+            text:'A man with no family name!',
+            pic:'../img/profile.jpg'
+        }
+    ]
 });
